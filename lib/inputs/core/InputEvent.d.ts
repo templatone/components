4f20cb3e0aeff551d15aa755f7685828dd9cd5f5
Adam Josefus <adamjosefus@gmail.com>
2021-05-31 08:53:11 +0200

    Update input events

--- lib/inputs/core/InputEvent.d.ts
+++ lib/inputs/core/InputEvent.d.ts
@@ -1,21 +1,21 @@
 declare const enum EventType {
-    FOCUS = "focus",
-    BLUR = "blur",
-    UPDATE = "update",
-    UPDATE_DELAYED = "update-delayed",
-    UPDATE_START = "update-start",
-    UPDATE_END = "update-end"
+    Focus = "focus",
+    Blur = "blur",
+    Update = "update",
+    UpdatePeriodically = "update-periodically",
+    UpdateStart = "update-start",
+    UpdateEnd = "update-end"
 }
 export declare class InputEvent<ValueType> extends CustomEvent<{
     value: ValueType;
     valid: boolean;
 }> {
-    static readonly FOCUS = EventType.FOCUS;
-    static readonly BLUR = EventType.BLUR;
-    static readonly UPDATE = EventType.UPDATE;
-    static readonly UPDATE_STABLE = EventType.UPDATE_DELAYED;
-    static readonly UPDATE_START = EventType.UPDATE_START;
-    static readonly UPDATE_END = EventType.UPDATE_END;
+    static readonly Focus = EventType.Focus;
+    static readonly Blur = EventType.Blur;
+    static readonly Update = EventType.Update;
+    static readonly UpdatePeriodically = EventType.UpdatePeriodically;
+    static readonly UpdateStart = EventType.UpdateStart;
+    static readonly UpdateEnd = EventType.UpdateEnd;
     constructor(typeArg: EventType, value: ValueType, isValid: boolean);
 }
 export {};
